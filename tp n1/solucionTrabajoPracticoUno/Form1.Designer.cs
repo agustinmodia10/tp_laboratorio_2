6dd12f43c155c1e8807cc72ee2597cb948084753
agustin modia <agustinmodiamoleracing@gmail.com>
2018-09-17 03:02:57 -0300

    h

---
+++ tp n1/solucionTrabajoPracticoUno/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace solucionTrabajoPracticoUno
+{
+    partial class frmCalculadora
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.cmbOperando = new System.Windows.Forms.ComboBox();
+            this.txtNumeroUno = new System.Windows.Forms.TextBox();
+            this.txtNumeroDos = new System.Windows.Forms.TextBox();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnOperar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(13, 13);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(61, 13);
+            this.lblResultado.TabIndex = 0;
+            this.lblResultado.Text = "-Resultado-";
+            this.lblResultado.Click += new System.EventHandler(this.lblResultado_Click);
+            // 
+            // cmbOperando
+            // 
+            this.cmbOperando.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbOperando.FormattingEnabled = true;
+            this.cmbOperando.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.cmbOperando.Location = new System.Drawing.Point(104, 37);
+            this.cmbOperando.Name = "cmbOperando";
+            this.cmbOperando.Size = new System.Drawing.Size(73, 21);
+            this.cmbOperando.TabIndex = 1;
+            this.cmbOperando.SelectedIndexChanged += new System.EventHandler(this.cmbOperando_SelectedIndexChanged);
+            // 
+            // txtNumeroUno
+            // 
+            this.txtNumeroUno.Location = new System.Drawing.Point(16, 37);
+            this.txtNumeroUno.Name = "txtNumeroUno";
+            this.txtNumeroUno.Size = new System.Drawing.Size(82, 20);
+            this.txtNumeroUno.TabIndex = 2;
+            this.txtNumeroUno.TextChanged += new System.EventHandler(this.txtNumeroUno_TextChanged);
+            // 
+            // txtNumeroDos
+            // 
+            this.txtNumeroDos.Location = new System.Drawing.Point(183, 37);
+            this.txtNumeroDos.Name = "txtNumeroDos";
+            this.txtNumeroDos.Size = new System.Drawing.Size(82, 20);
+            this.txtNumeroDos.TabIndex = 3;
+            this.txtNumeroDos.TextChanged += new System.EventHandler(this.txtNumeroDos_TextChanged);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(52, 76);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 4;
+            this.btnLimpiar.Text = "CC";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnOperar
+            // 
+            this.btnOperar.Location = new System.Drawing.Point(160, 76);
+            this.btnOperar.Name = "btnOperar";
+            this.btnOperar.Size = new System.Drawing.Size(75, 23);
+            this.btnOperar.TabIndex = 5;
+            this.btnOperar.Text = "=";
+            this.btnOperar.UseVisualStyleBackColor = true;
+            this.btnOperar.Click += new System.EventHandler(this.btnOperar_Click);
+            // 
+            // frmCalculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnOperar);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.txtNumeroDos);
+            this.Controls.Add(this.txtNumeroUno);
+            this.Controls.Add(this.cmbOperando);
+            this.Controls.Add(this.lblResultado);
+            this.Name = "frmCalculadora";
+            this.Text = "Calculadora";
+            this.Load += new System.EventHandler(this.frmCalculadora_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.ComboBox cmbOperando;
+        private System.Windows.Forms.TextBox txtNumeroUno;
+        private System.Windows.Forms.TextBox txtNumeroDos;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnOperar;
+    }
+}
+
---
+++ tp n1/solucionTrabajoPracticoUno/Numero.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solucionTrabajoPracticoUno
+{
+    public class Numero
+    {
+        private double _numero;
+
+        #region Constructores
+
+        public Numero()
+        {
+            this._numero = 0;
+        }
+
+        public Numero(string numero)
+        {
+            this.setNumero(numero);
+        }
+
+        public Numero(double numero)
+        {
+            this._numero = numero;
+        }
+
+        #endregion
+
+        #region Metodos de la clase
+
+        public double getNumero()
+        {
+            return this._numero;
+        }
+
+        private void setNumero(string numero)
+        {
+            this._numero = validarNumero(numero);
+        }
+
+        private double validarNumero(string numeroString)
+        {
+            double numeroValido;
+
+            if (double.TryParse(numeroString, out numeroValido))
+            {
+                return numeroValido;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        #endregion
+
+        #region Sobrecargas de operadores
+
+        public static implicit operator Numero(double numero)
+        {
+            return new Numero(numero);
+        }
+
+        public static double operator +(Numero numeroUno, Numero numeroDos)
+        {
+            double resultado;
+
+            resultado = numeroUno._numero + numeroDos._numero;
+
+            return resultado;
+        }
+
+        public static double operator *(Numero numeroUno, Numero numeroDos)
+        {
+            double resultado;
+
+            resultado = numeroUno._numero * numeroDos._numero;
+
+            return resultado;
+        }
+
+        public static double operator -(Numero numeroUno, Numero numeroDos)
+        {
+            double resultado;
+
+            resultado = numeroUno._numero - numeroDos._numero;
+
+            return resultado;
+        }
+
+        public static double operator /(Numero numeroUno, Numero numeroDos)
+        {
+            double resultado;
+
+            resultado = numeroUno._numero / numeroDos._numero;
+
+            return resultado;
+        }
+
+        public static bool operator >(Numero numeroUno, Numero numeroDos)
+        {
+            if (numeroUno._numero > numeroDos._numero)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator <(Numero numeroUno, Numero numeroDos)
+        {
+            if (numeroUno._numero < numeroDos._numero)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator ==(Numero numeroUno, Numero numeroDos)
+        {
+            if (numeroUno._numero == numeroDos._numero)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator !=(Numero numeroUno, Numero numeroDos)
+        {
+            if (numeroUno._numero != numeroDos._numero)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator ==(Numero numeroUno, double numero)
+        {
+            if (numeroUno._numero == numero)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator !=(Numero numeroUno, double numero)
+        {
+            if (numeroUno._numero == numero)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        #endregion
+    }
+}
